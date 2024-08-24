8d684bd45ce62d1db11b400b58513aac2f544b89
José David Serna <83099671+JDSerna00@users.noreply.github.com>
2024-08-23 20:43:25 -0500

    ImportingPackages

---
+++ Assets/Wizard - 2D Character/Demo/SimplePlayerController.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+
+namespace ClearSky
+{
+    public class SimplePlayerController : MonoBehaviour
+    {
+        public float movePower = 10f;
+        public float speed = 5f;
+        public float jumpPower = 15f; //Set Gravity Scale in Rigidbody2D Component to 5
+
+        private Rigidbody2D rb;
+        private Animator anim;
+        Vector3 movement;
+        public Joystick joystick;
+        private int direction = 1;
+        bool isJumping = false;
+        private bool alive = true;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            anim = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            Restart();
+            if (alive)
+            {
+                Hurt();
+                Die();
+                Attack();
+                Jump();
+                Run();
+
+            }
+        }
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            anim.SetBool("isJump", false);
+        }
+
+
+        void Run()
+        {
+            float horizontalInput = joystick.Horizontal;
+            Vector3 move = new Vector3(horizontalInput, 0, 0) * speed;
+            anim.SetBool("isRun", false);
+
+
+            if (horizontalInput < 0)
+            {
+                direction = -1;
+                move = Vector3.left;
+
+                transform.localScale = new Vector3(direction, 1, 1);
+                if (!anim.GetBool("isJump"))
+                    anim.SetBool("isRun", true);
+
+            }
+            if (horizontalInput > 0)
+            {
+                direction = 1;
+                move = Vector3.right;
+
+                transform.localScale = new Vector3(direction, 1, 1);
+                if (!anim.GetBool("isJump"))
+                    anim.SetBool("isRun", true);
+
+            }
+            transform.position += move * movePower * Time.deltaTime;
+        }
+        public void Jump()
+        {
+            if (!anim.GetBool("isJump"))
+            {
+                isJumping = true;
+                anim.SetBool("isJump", true);
+            }
+            if (!isJumping)
+            {
+                return;
+            }
+
+            rb.velocity = Vector2.zero;
+
+            Vector2 jumpVelocity = new Vector2(0, jumpPower);
+            rb.AddForce(jumpVelocity, ForceMode2D.Impulse);
+
+            isJumping = false;
+        }
+        void Attack()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                anim.SetTrigger("attack");
+            }
+        }
+        void Hurt()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                anim.SetTrigger("hurt");
+                if (direction == 1)
+                    rb.AddForce(new Vector2(-5f, 1f), ForceMode2D.Impulse);
+                else
+                    rb.AddForce(new Vector2(5f, 1f), ForceMode2D.Impulse);
+            }
+        }
+        void Die()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                anim.SetTrigger("die");
+                alive = false;
+            }
+        }
+        void Restart()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0))
+            {
+                anim.SetTrigger("idle");
+                alive = true;
+            }
+        }
+    }
+}
